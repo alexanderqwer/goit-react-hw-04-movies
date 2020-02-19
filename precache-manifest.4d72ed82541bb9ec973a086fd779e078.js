cee1ffbd550ba5549b70e9c68f60ac5c0b527fad
Alexanderqwer <llkkfi27456@gmail.com>
2020-02-19 15:59:26 +0200

    Updates

---
+++ precache-manifest.4d72ed82541bb9ec973a086fd779e078.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2471da8b83e752119b036bea10697a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c328df62040b945b5077",
+    "url": "/goit-react-hw-02-feedback/static/js/0.b2be25b5.chunk.js"
+  },
+  {
+    "revision": "3bf0380f89299d0d6902",
+    "url": "/goit-react-hw-02-feedback/static/js/8.74b2784a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-02-feedback/static/js/8.74b2784a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7e5e0d92bc859e5c237",
+    "url": "/goit-react-hw-02-feedback/static/js/Cast.b7c1fd21.chunk.js"
+  },
+  {
+    "revision": "88fffa51605102eedfeb",
+    "url": "/goit-react-hw-02-feedback/static/js/HomePage.6d90d9c3.chunk.js"
+  },
+  {
+    "revision": "4ee46d1a023a2dfee664",
+    "url": "/goit-react-hw-02-feedback/static/js/MovieDetalisPage.a949a01d.chunk.js"
+  },
+  {
+    "revision": "fe05b716dd7c1265ee77",
+    "url": "/goit-react-hw-02-feedback/static/js/MoviePage.bf9c04e1.chunk.js"
+  },
+  {
+    "revision": "c532ae0c1ec5a8a27280",
+    "url": "/goit-react-hw-02-feedback/static/js/Reviews.dd7395a3.chunk.js"
+  },
+  {
+    "revision": "88b780744585aba9a9f3",
+    "url": "/goit-react-hw-02-feedback/static/js/main.d1ac1715.chunk.js"
+  },
+  {
+    "revision": "a5888b8b0f123725a3dd",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.189a1dee.js"
+  }
+]);
